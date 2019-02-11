280d9f38d1e9b5be8d328e02e3eac42432fb584d
iyinraphael <iyinraphael@gmail.com>
2019-02-11 13:09:17 -0800

    created unit test target

---
+++ AstronomyTests/AstronomyTests.swift
@@ -0,0 +1,13 @@
+//
+//  AstronomyTests.swift
+//  AstronomyTests
+//
+//  Created by Iyin Raphael on 2/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class AstronomyTests: XCTestCase {
+
+}
